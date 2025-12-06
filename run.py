fd299d76b7581cff53e239f0eee039bce899851e
ikopmniko <hannapitman786@gmail.com>
2025-12-06 13:38:13 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,15 +3,15 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 
 # API Serper
-API_KEY = "3f4a67e84f6d9790d8286c6aa7558446c6e098ff"
+API_KEY = "4eaf4a73bb0ff19c0b37641149940628c5e53647"
 URL_API = "https://google.serper.dev/search"
 
 # Endpoint PHP kamu
 PHP_ENDPOINT = "https://leamarie-yoga.de/save_serper.php"
 
 # Rentang baris
-mulai = 32500
-endnya = 35000
+mulai = 35000
+endnya = 37500
 
 headers = {
     "X-API-KEY": API_KEY,
@@ -78,6 +78,7 @@ def main(start_line=1, end_line=None):
 
 if __name__ == "__main__":
     main(start_line=mulai, end_line=endnya)
+
 
 
 
